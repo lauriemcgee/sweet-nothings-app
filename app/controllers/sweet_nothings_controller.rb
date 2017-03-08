25f09070025e42bc933592c1ba925f959d9993e6
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-07 21:28:58 -0600

    using each loop rather than accessing each variable

--- app/controllers/sweet_nothings_controller.rb
+++ app/controllers/sweet_nothings_controller.rb
@@ -1,11 +1,10 @@
 class SweetNothingsController < ApplicationController
   def sweet_nothings_method
-    @sweet_nothing1 = SweetNothing.first
-    @sweet_nothing2 = SweetNothing.second
-    @sweet_nothing3 = SweetNothing.third
-    @sweet_nothing4 = SweetNothing.fourth
-    @sweet_nothing5 = SweetNothing.fifth
-    @sweet_nothing6 = SweetNothing.sixth
+    # @sweet_nothing1 = SweetNothing.first
+    # @sweet_nothing2 = SweetNothing.second
+    # @sweet_nothing3 = SweetNothing.third
+    # @sweet_nothing4 = SweetNothing.fourth
+    # @sweet_nothing5 = SweetNothing.fifth
     @sweet_nothings = SweetNothing.all
     render "sweet_nothings.html.erb"  
   end
