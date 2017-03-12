eb2eb08d6121499b3e855d46dddff25e9c8206a9
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-12 11:06:18 -0500

    needs to be fixed, add create, show, edit, and update functions

--- app/controllers/sweet_nothings_controller.rb
+++ app/controllers/sweet_nothings_controller.rb
@@ -1,11 +1,37 @@
 class SweetNothingsController < ApplicationController
-  def sweet_nothings_method
-    # @sweet_nothing1 = SweetNothing.first
-    # @sweet_nothing2 = SweetNothing.second
-    # @sweet_nothing3 = SweetNothing.third
-    # @sweet_nothing4 = SweetNothing.fourth
-    # @sweet_nothing5 = SweetNothing.fifth
+  def index
     @sweet_nothings = SweetNothing.all
-    render "sweet_nothings.html.erb"  
+    render "index.html.erb"  
+  end
+  def new
+    render "new.html.erb"
+  end
+  def create
+    sweet_nothing = SweetNothing.new(
+      name: params[:form_name],
+      image: params[:form_image],
+      description: params[:form_description]
+      )
+    sweet_nothing.save
+    render "create.html.erb"
+  end
+  def show
+    @sweet_nothing_id = params[:id]
+    @sweet_nothing = SweetNothing.find_by[id: sweet_nothing_id]
+    render "show.html.erb"
+  end
+  def edit
+    sweet_nothing_id = params[:id]
+    @sweet_nothing = SweetNothing.find_by(id: sweet_nothing_id)
+    render "edit.html.erb"
+  end
+  def update
+    sweet_nothing_id = params[:id]
+    @sweet_nothing = SweetNothing.find_by(id: sweet_nothing_id)    
+    @sweet_nothing.name = params[:form_name]
+    @sweet_nothing.image = params[:form_image]
+    @sweet_nothing.description = params[:form_description]
+    @sweet_nothing.save
+    render "update.html.erb"
   end
 end
